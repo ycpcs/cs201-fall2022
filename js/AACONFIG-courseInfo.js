6e2633ec1407e3fdb795067dcf3a09e237c51db4
Krishna <kkambhampaty@ycp.edu>
2022-08-22 03:55:56 +0100

    changed courseInfo to reflect 103 section

--- js/AACONFIG-courseInfo.js
+++ js/AACONFIG-courseInfo.js
@@ -6,7 +6,7 @@ var PREPOPULATE = true;
 
 var courseInfo = {
 	courseName: "CS 201: Fundamentals of Computer Science II",
-	classDays: ["Wed", "Fri"],
+	classDays: ["Tue", "Thur"],
 
 	// The following is true if final exam is on the LAST day of class;
 	// false if final exam is during exam week.
